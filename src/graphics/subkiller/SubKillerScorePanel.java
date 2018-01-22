dd4ab0d36b07fffd70bb655e6986a6af0c10c2ef
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-01-22 09:58:51 -0800

    subkiller scorepanel

--- src/graphics/subkiller/SubKillerScorePanel.java
+++ src/graphics/subkiller/SubKillerScorePanel.java
@@ -1,8 +1,25 @@
 package graphics.subkiller;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * SubKillerScorePanel
  */
-public class SubKillerScorePanel {
-	
+public class SubKillerScorePanel extends JPanel {
+	private int score = 0;
+
+	public SubKillerScorePanel() {
+		this.setLayout(new GridLayout(1,2));
+
+		JPanel difficulty = new JPanel();
+		JLabel diffLabel = new JLabel("Difficulty: ");
+		JSlider diffSlider = new JSlider(1,5,1);
+
+		diffSlider.setMajorTickSpacing(2);
+		diffSlider.setMinorTickSpacing(1);
+
+		difficulty.add(diffLabel);
+		difficulty.add(diffSlider);
+	}
 }
