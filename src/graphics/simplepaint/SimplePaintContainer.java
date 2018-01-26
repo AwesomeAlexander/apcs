a9dda32f51b59b058c5b3695ec3cebb5a072fb7a
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-01-25 21:45:36 -0800

    simplepaint

--- src/graphics/simplepaint/SimplePaintContainer.java
+++ src/graphics/simplepaint/SimplePaintContainer.java
@@ -1,6 +1,18 @@
 package graphics.simplepaint;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 /**
  * SimplePaintContainer
@@ -8,9 +20,57 @@ import javax.swing.JPanel;
 public class SimplePaintContainer extends JPanel {
 	static final long serialVersionUID = 42L;
 
+	SimplePaintPanel drawingSpace;
+	SimplePaintListener listener;
+	JMenuBar menu;
+	JPanel buttonPanel;
+
 	public SimplePaintContainer() {
 		super();
 
+		// Object declaration
+
+		this.menu = new JMenuBar();
+		this.drawingSpace = new SimplePaintPanel();
+		this.buttonPanel = new JPanel();
+		this.listener = new SimplePaintListener(this.drawingSpace);
+
+		// Menu Stuffs
+
+		JMenu simplepaint = new JMenu("Simple Paint");
+		JMenu edit = new JMenu("Edit");
+
+		simplepaint.add(new JMenuItem("Quit")).addActionListener(listener);
+		edit.add(new JMenuItem("Clear")).addActionListener(listener);
+		edit.add(new JMenuItem("Undo")).addActionListener(listener);
+
+		menu.add(simplepaint);
+		menu.add(edit);
 		
+		// Button Stuffs
+
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+		for (String s : "Red, Green, Blue, Cyan, Magenta, Yellow, Custom, Clear".split(", "))
+			buttonPanel.add(new JButton(s) {
+				static final long serialVersionUID = 42L;
+				{
+					setSize(200,100);
+					setMaximumSize(getSize());
+					setAlignmentX(Component.CENTER_ALIGNMENT);
+					addActionListener(listener);
+				}
+			});
+
+		// Listener registration
+
+		this.drawingSpace.addMouseListener(this.listener);
+		this.drawingSpace.addMouseMotionListener(this.listener);
+
+		// Adding components to master container
+
+		this.setLayout(new BorderLayout());
+		this.add(this.menu, BorderLayout.NORTH);
+		this.add(this.drawingSpace, BorderLayout.CENTER);
+		this.add(this.buttonPanel, BorderLayout.EAST);
 	}
 }
--- src/graphics/simplepaint/SimplePaintListener.java
+++ src/graphics/simplepaint/SimplePaintListener.java
@@ -1,9 +1,13 @@
 package graphics.simplepaint;
 
 import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.JColorChooser;
 
 import graphics.drawings.Line;
 
+import java.awt.Color;
 import java.awt.Point;
 
 /**
@@ -19,7 +23,27 @@ public class SimplePaintListener extends MouseAdapter implements ActionListener 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO: buttons and menu listeneing
+		String action = e.getActionCommand().toUpperCase();
+
+		if (getColor(action) != null) {
+			panel.color = getColor(action);
+			return;
+		}
+		switch (action) {
+			case "QUIT": System.exit(0);
+			case "UNDO":
+				panel.lines.remove(panel.lines.size()-1);
+				panel.repaint();break;
+			case "CLEAR":
+				panel.lines = new ArrayList<Line>();
+				panel.repaint();break;
+			case "CUSTOM":
+				panel.color = JColorChooser.showDialog(panel, "Choose a Custom Color to draw with!", panel.color);
+				if (panel.color==null) panel.color = Color.WHITE;
+				break;
+			default:
+				System.out.println("Got an unhandled''"+action+"'' command.");
+		}
 	}
 
     public void mousePressed(MouseEvent e) {
@@ -40,6 +64,18 @@ public class SimplePaintListener extends MouseAdapter implements ActionListener 
 		    panel.lines.add(new Line(panel.prev.x,panel.prev.y,e.getX(),e.getY(),panel.color));
         panel.prev = new Point(e.getX(),e.getY());
         
-        panel.repaint();
-    }
+		panel.repaint();
+	}
+	
+	public Color getColor(String thecolor) {
+		switch (thecolor.toUpperCase()) {
+			case "RED": return Color.RED;
+			case "GREEN": return Color.GREEN;
+			case "BLUE": return Color.BLUE;
+			case "CYAN": return Color.CYAN;
+			case "MAGENTA": return Color.MAGENTA;
+			case "YELLOW": return Color.YELLOW;
+			default: return null;
+		}
+	}
 }
