28693524c15954ea8caf64890e6b8e63dd7eba18
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-03-06 22:50:34 -0800

    Did some messing around with .classpath

---
+++ src/algorithms/testing/SomethingTest.java
@@ -0,0 +1,18 @@
+package algorithms.testing;
+
+public class SomethingTest {
+
+    int num;
+
+
+    public void setUp() {
+        System.out.println("Setting up!");
+        
+        num = 4;
+    }
+
+
+    public void testOne() {
+        //Assert.assertEquals(14, Something.add10(num));
+    }
+}
