aa40b063912c330d27da4592989762b7dec51276
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-03-15 18:48:51 -0700

    humansort

--- src/algorithms/SortLibrary.java
+++ src/algorithms/SortLibrary.java
@@ -1,4 +1,4 @@
-// package algorithms;
+package algorithms;
 
 import java.util.Arrays;
 
@@ -11,24 +11,24 @@ public class SortLibrary {
 		int[] alreadySorted = new int[]{2,9,11,15,28,33,40,47,51,64,76,77,82,85,94};
 		int[] reversed = new int[]{94,85,82,77,76,64,51,47,40,33,28,15,11,9,2};
 		int[] mostlySorted = new int[]{2,85,11,15,28,33,47,40,51,64,76,77,82,9,94};
-		//int[] longerArray = ArrayImporter.readArrayFile("smallArray.txt"); 
+		int[] longerArray = ArrayImporter.readArrayFile("smallArray.txt"); 
 		int[] myCustomTest = new int[]{15, 23, 42, 4, 8, 16, 1};	
 		
 		
 		// ***Enter your array to sort here
 		int[] arrayToSort = random; // arrayToSort will point to the array you choose
-		int[] copyOfArrayToSort = Arrays.copyOf(arrayToSort, arrayToSort.length);
+		int[] checkerArrayToSort = Arrays.copyOf(arrayToSort, arrayToSort.length);
 		
 		// ***Enter which sort you want to test
-		quickSort(arrayToSort);		// Call your sort method -- Remember array is modified in the method, not returned!
-		Arrays.sort(copyOfArrayToSort);	// call java.util.Array's sort method for comparison
+		humanSort(arrayToSort);		// Call your sort method -- Remember array is modified in the method, not returned!
+		Arrays.sort(checkerArrayToSort);	// call java.util.Array's sort method for comparison
 
 		if(arrayToSort.length < 50) {
 			System.out.println("Result after sort: " + Arrays.toString(arrayToSort));
-			System.out.println("Result should be: " + Arrays.toString(copyOfArrayToSort));
+			System.out.println("Result should be: " + Arrays.toString(checkerArrayToSort));
 		}
 		
-		System.out.println("Sorts match? " + Arrays.equals(arrayToSort, copyOfArrayToSort));
+		System.out.println("Sorts match? " + Arrays.equals(arrayToSort, checkerArrayToSort));
 		
 	}
 	
@@ -90,17 +90,17 @@ public class SortLibrary {
 		return merged;
 	}
 
-	public static void quickSort(int[] nums) {quickSort(nums,(int)(Math.random()*(nums.length-0)+0),0,nums.length-1);}
-	public static void quickSort(int[] nums,int pivotInd,int low,int high) {
+	public static void quickSort(int[] nums) {quickSort(nums,0,nums.length-1);}
+	public static void quickSort(int[] nums,int low,int high) {
 		if (low >= high || (low < 0 || high > nums.length)) return;
 
-		int pivot = nums[pivotInd];
-		System.out.println("Called with low: "+low+", high: "+high+", and Pivot: nums["+pivotInd+"] = "+pivot);
+		int pivotInd = (int)(Math.random()*(high-low+1)+low), pivot = nums[pivotInd];
+		// System.out.println("Called with low: "+low+",high: "+high+",and Pivot: nums["+pivotInd+"] = "+pivot);
 
 		// Iterating through partition, sorting to lower and higher partitions
-		for (int i=low,j=high;i <= j;) {
-			while (nums[i] < pivot && i<=j) i++;
-			while (nums[j] > pivot && j>=i) j--;
+		for (int i=low,j=high;i < j;) {
+			while (nums[i] < pivot && i<j) i++;
+			while (nums[j] > pivot && j>i) j--;
 
 			if (nums[i] == pivot) pivotInd = i;
 			if (nums[j] == pivot) pivotInd = j;
@@ -108,8 +108,8 @@ public class SortLibrary {
 			swapInArr(nums,i,j);
 		}
 
-		quickSort(nums,(int)(Math.random()*(high-low+1)+low),low,pivotInd-1);
-		quickSort(nums,(int)(Math.random()*(high-low+1)+low),pivotInd+1,high);
+		quickSort(nums,low,pivotInd-1);
+		quickSort(nums,pivotInd+1,high);
 	}
 
 	public static void humanSort(int[] nums) {
@@ -122,10 +122,21 @@ public class SortLibrary {
 			min = Math.min(nums[i],min);
 			max = Math.max(nums[i],max);
 		}
+		System.out.println("Found min as "+min+" and max as "+max);
+
+		System.out.println("Array indices range from 0 to "+(out.length-1));
 
 		// Sorting
+		double place;
 		for (int i=0;i<nums.length;i++) {
-			out[/*function here*/ 0] = nums[i];
+			// The place to put it is value out of range 0-1 then scaled to array length.
+			place = (1.0 * (nums[i]-min)/(max-min) * (nums.length-1));
+			System.out.println("Num: "+nums[i]+" | Old: "+i+" | New: "+place+" -> " + Math.round(place));
+			if (out[(int)Math.round(place)] != 0) {
+				out[(int)Math.round(place)] = nums[i];
+			} else {
+
+			}
 		}
 
 		// (Optional) Copying over to nums, void function
